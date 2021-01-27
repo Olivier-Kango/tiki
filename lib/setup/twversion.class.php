6d0079795bb965431c55662b0a00228156ee80ee
Roberto Kirschbaum <robertokir.pro@gmail.com>
2021-01-27 16:13:32 +0000

    [REL] Closing 22.1 release

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -28,7 +28,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '22.1';	// needs to have no spaces for releases
+		$this->version 	= '22.2vcs';	// needs to have no spaces for releases
 		$this->star		= 'Corona Borealis';
 		$this->releases	= [];
 
