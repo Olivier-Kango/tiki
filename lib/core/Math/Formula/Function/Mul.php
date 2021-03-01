a686da004282e95c869ce819b4c3acbe0bb7d7d0
Jonny Bradley <jmail@nospaces.net>
2021-03-01 18:54:53 +0000

    [FIX] calc: Move casting to numeric in MUL function to where it's needed (fixed for php8 but broke a test)

--- lib/core/Math/Formula/Function/Mul.php
+++ lib/core/Math/Formula/Function/Mul.php
@@ -26,10 +26,10 @@ class Math_Formula_Function_Mul extends Math_Formula_Function
 		} else {
 			$initial = $this->firstOrApplicator($list);
 			return array_reduce($list, function($carry, $item) {
-				$item = ($item == (int) $item) ? (int) $item : (float) $item;
 				if ($carry instanceof Math_Formula_Applicator) {
 					return $carry->mul($item);
 				} else {
+					$item = ($item == (int) $item) ? (int) $item : (float) $item;
 					$carry = ($carry == (int) $carry) ? (int) $carry : (float) $carry;
 					return $carry * $item;
 				}
