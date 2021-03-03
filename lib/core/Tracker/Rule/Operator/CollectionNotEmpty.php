d7f0ed863761a050e228fad30910ef7f98f27e4a
Jonny Bradley <jmail@nospaces.net>
2021-03-03 16:52:35 +0000

    [ENH] tracker-rules: Add missing "collection not empty" operator

---
+++ lib/core/Tracker/Rule/Operator/CollectionNotEmpty.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Tiki\Lib\core\Tracker\Rule\Operator;
+
+
+use Tiki\Lib\core\Tracker\Rule\Type\Collection;
+use Tiki\Lib\core\Tracker\Rule\Type\Nothing;
+
+class CollectionNotEmpty extends Operator
+{
+	function __construct()
+	{
+		parent::__construct(tr('is not empty'), Nothing::class, '.val().length>0', [Collection::class]);
+	}
+}
