b97d8dcc1b654da8907258dc70beb1022965dc50
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2021-03-10 09:10:13 -0300

    [FIX] Make composer test script correctly launch phpunit

--- lib/test/phpunit.php
+++ lib/test/phpunit.php
@@ -1,23 +1,28 @@
-<?php
+<?php declare(strict_types=1);
 // (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id$
 
-/**
-* A simple wrapper around /usr/bin/phpunit to enable debugging
-* tests with Xdebug from within Aptana or Eclipse.
-*
-* Linux only (it should be simple to add support to other OSs).
-*/
+// This is a modified version of vendor_bundled/vendor/phpunit/phpunit/phpunit. The
+// reason to this file exists is phpunit seems to have problem when reading relative
+// paths for the parameter `-c` or `--configuration`. This file set the current
+// directory to TIKI_PATH before starting phpunit, and make possible to run 
+// `composer test -d vendor_bundled` again.
+// https://github.com/sebastianbergmann/phpunit/issues/552
 
-echo "Please use 'php phpunit' instead" . PHP_EOL;
+if (!ini_get('date.timezone')) {
+    ini_set('date.timezone', 'UTC');
+}
 
-// Linux
-//require_once(__DIR__ . '/../../vendor_bundled/vendor/phpunit/phpunit/phpunit');
+chdir(dirname(dirname(__DIR__)));
+define('PHPUNIT_COMPOSER_INSTALL', implode(DIRECTORY_SEPARATOR, ['vendor_bundled', 'vendor', 'autoload.php']));
 
-// Windows
-// comment out the Linux require line (above) and uncomment the 2 lines below
-//$pear_bin_path = getenv('PHP_PEAR_BIN_DIR').DIRECTORY_SEPARATOR;
-//require_once($pear_bin_path."phpunit");
+$options = getopt('', array('prepend:'));
+if (isset($options['prepend'])) {
+    require $options['prepend'];
+}
+unset($options);
+
+require PHPUNIT_COMPOSER_INSTALL;
+PHPUnit\TextUI\Command::main();
