db33082152df3d4a0f60e0fa10b85ff7f25a0521
Jonny Bradley <jmail@nospaces.net>
2021-07-21 17:33:11 +0100

    [FIX] imagegallery migration: Only show image gallery migration feedback to admins, and make the notice about successfully migrated galleries a warning with a more helpful note about how to fix the old image urls.

--- show_image.php
+++ show_image.php
@@ -14,17 +14,29 @@ if (! isset($_REQUEST["nocache"])) {
     session_cache_limiter('private_no_expire');
 }
 
+global $tiki_p_admin, $prefs, $tikilib, $tikipath;
+
 include_once("tiki-setup.php");
 
 if ($prefs['feature_file_galleries'] == 'y' && $prefs['file_galleries_redirect_from_image_gallery'] == 'y') {
-    $fileGalleryInfo = $tikilib->table('tiki_object_attributes')->fetchRow([], ['value' => $_REQUEST["id"], 'attribute' => 'tiki.file.imageid']);
-    if ($fileGalleryInfo) {
+
+    $fileInfo = $tikilib->table('tiki_object_attributes')->fetchRow([], ['value' => $_REQUEST["id"], 'attribute' => 'tiki.file.imageid']);
+    if ($fileInfo) {
         include_once($tikipath . 'tiki-sefurl.php');
-        TikiLib::lib('access')->redirect(filter_out_sefurl('tiki-download_file.php?fileId=' . $fileGalleryInfo['itemId'] . '&display'));
+        $newUrl = filter_out_sefurl('tiki-download_file.php?fileId=' . $fileInfo['itemId'] . '&display');
+
+        if ($tiki_p_admin === 'y') {
+            Feedback::warning(tr('Image Galleries have been migrated but show_image.php is still in use.%0 Change "%1" to "%2"',
+                '<br>', 'show_image.php?id=' . $_REQUEST['id'], $newUrl
+            ));
+        }
+
+        TikiLib::lib('access')->redirect($newUrl);
+
+    } else {
+        Feedback::error(tr('File info not found for migrated image gasllery file %0', $_REQUEST["id"]));
     }
+} else if ($tiki_p_admin === 'y') {
+    Feedback::error(tr('Image Galleries have been removed. Run the migration script: `php console.php gallery:migrate`'));
 }
 
-// TODO ImageGalleryRemoval23.x remove this eventually?
-// Kept redirect to filegal
-
-Feedback::error(tr('Image Galleries have been removed. Run the migration script: ` php console.php gallery:migrate`'));
