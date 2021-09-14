Olivier-Kango/tiki

commit 77749de0d4753699ffdf949bc454b2b31d629cca

[FIX] Fix issues with diagrams
- Fix issue with escaped diagram content when creating a new one;
- Wikiplugin diagram content is now converted to MD5 when Tiki is checking for version differences;

lib/core/File/DiagramHelper.php
@@ -8,12 +8,14 @@
 
 namespace Tiki\File;
 
+use Feedback;
 use Tiki\FileGallery\File as TikiFile;
 use Tiki\FileGallery\File;
 use Tiki\Package\VendorHelper;
 use Tiki\Process\Process;
 use TikiLib;
 use WikiParser_PluginArgumentParser;
+use WikiParser_PluginMatcher;
 
 class DiagramHelper
 {
@@ -22,6 +24,7 @@ class DiagramHelper
 
     const WIKI_SYNTAX_DEFAULT_DIAGRAM_MXCELL_REPEAT = 'vertical';
     const WIKI_SYNTAX_DEFAULT_DIAGRAM_MXCELL_OFFSET = '10';
+    const WIKI_SYNTAX_DIAGRAM_PLUGIN = 'diagram';
 
     /**
      * Get diagram as image given a file ID or diagram contents.
@@ -73,8 +76,12 @@ class DiagramHelper
 
             $rawXmlContent = $file->getContents();
         }
-
         $diagramRoot = simplexml_load_string($rawXmlContent);
+
+        if ($diagramRoot === false && ! empty($identifier)) {
+            Feedback::error(tr('The provided diagram XML is not valid. Please check and validate the diagram structure.'));
+        }
+
         $diagrams = [];
 
         foreach ($diagramRoot->diagram as $diagram) {
@@ -229,6 +236,32 @@ class DiagramHelper
         }
 
         return self::deflate($rootDiagram);
+    }
+
+    /**
+     * Converts a diagram plugin inner content to md5 (useful when performing diffs)
+     * @param WikiParser_PluginMatcher $parser
+     * @return bool returns true if any content was changed
+     */
+    public static function md5WikiPluginDiagramContent(WikiParser_PluginMatcher &$parser): bool
+    {
+        $hasChanges = false;
+        $parser->rewind();
+        while ($parser->valid()) {
+            $current = $parser->current();
+
+            if ($parser->current()->getName() !== self::WIKI_SYNTAX_DIAGRAM_PLUGIN || empty($parser->current()->getBody())) {
+                continue;
+            }
+
+            $bodyHash = tr('diagram content hash - %0', md5($current->getBody()));
+            $current->replaceWithPlugin(self::WIKI_SYNTAX_DIAGRAM_PLUGIN, $current->getArguments(), $bodyHash);
+
+            $hasChanges = true;
+            $parser->next();
+        }
+
+        return $hasChanges;
     }
 
     /**

lib/tikilib.php
@@ -6,6 +6,7 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
+use Tiki\File\DiagramHelper;
 use Tiki\TikiInit;
 
 require_once(__DIR__ . '/../lib/debug/Tracer.php');
@@ -5345,7 +5346,23 @@ class TikiLib extends TikiDb_Bridge
                 $old = $histlib->get_version($pageName, $old_version);
                 $foo = parse_url($_SERVER["REQUEST_URI"]);
                 $machine = self::httpPrefix(true) . dirname($foo["path"]);
-                $diff = diff2($old["data"], $edit_data, "unidiff"); // TODO: Only compute if we have at least one notification to send
+
+                $oldPagePlugins = WikiParser_PluginMatcher::match($old["data"]);
+                $editedPagePlugins = WikiParser_PluginMatcher::match($edit_data);
+
+                // Diagram data can be too big to diff, convert the content to md5 that will help to check for differences
+                $replacedOldPagePlugins = DiagramHelper::md5WikiPluginDiagramContent($oldPagePlugins);
+                $replacedEditedPagePlugins = DiagramHelper::md5WikiPluginDiagramContent($editedPagePlugins);
+
+                $oldPagePlugins->rewind();
+                $editedPagePlugins->rewind();
+
+                $parsedOldPage = $oldPagePlugins->getText();
+                $parsedNewPage = $editedPagePlugins->getText();
+
+                TikiLib::lib('smarty')->assign('has_md5_content_diagrams', $replacedOldPagePlugins || $replacedUpdatedPagePlugins);
+
+                $diff = diff2($parsedOldPage, $parsedNewPage, "unidiff"); // TODO: Only compute if we have at least one notification to send
                 sendWikiEmailNotification('wiki_page_changed', $pageName, $edit_user, $edit_comment, $old_version, $edit_data, $machine, $diff, $edit_minor, $hash['contributions'], 0, 0, $lang);
             }
         }

lib/wiki-plugins/wikiplugin_diagram.php
@@ -264,7 +264,7 @@ XML;
                 <input type="hidden" name="page" value="$page"/>
                 <input type="hidden" name="template" value="$template"/>
                 <input type="hidden" name="fileName" value="$fileName"/>
-                <input type="hidden" name="xml" value="$data"/>
+                <input type="hidden" name="xml" value='$data'/>
                 <input type="hidden" name="index" value="$diagramIndex"/>
             </p>
         </form>

templates/mail/user_watch_wiki_page_changed.tpl
@@ -27,6 +27,9 @@
 
 ***********************************************************
 {if $mail_diffdata}{tr}The changes in this version follow below, followed after by the current full page text.{/tr}
+{if $has_md5_content_diagrams}
+{tr}Diagram plugin content was replaced with MD5 hash for version comparison.{/tr}
+{/if}
 ***********************************************************
 
 {section name=ix loop=$mail_diffdata}

commit d4693c4839120431f539485b0aee18a352a575e6

[FIX] wiki: Make it possible to save pages without diagram plugins in again (regression from 77749de0)

lib/core/File/DiagramHelper.php
@@ -251,6 +251,7 @@ class DiagramHelper
             $current = $parser->current();
 
             if ($parser->current()->getName() !== self::WIKI_SYNTAX_DIAGRAM_PLUGIN || empty($parser->current()->getBody())) {
+                $parser->next();
                 continue;
             }
 