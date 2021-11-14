7e25d8da407f601b0f85dc6ab4eccc7621ba4085
Andon Andonov <andon.andonov85@gmail.com>
2021-11-14 11:04:39 +0000

    [UPD] Setting up production and development modes; Added documentation

--- lib/vue-mf/kanban/src/App.vue
+++ lib/vue-mf/kanban/src/App.vue
@@ -5,17 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Kanban Vue 3 + Vite" />
+    <img alt="Vue logo" src="./logo.png" />
+    <HelloWorld msg="Kanban Vue 3 + Vite" />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
