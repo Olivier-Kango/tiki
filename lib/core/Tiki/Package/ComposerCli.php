7bdc19119e3048950e88fa6064cadb17f8f896af
Jorge Sá Pereira <me@jorgesapereira.com>
2021-12-23 18:04:43 +0000

    [ENH] Set composer to not prepend vendor pacakges autoload
    - Packages from vendor_bundled have priority over vendor packages

--- lib/core/Tiki/Package/ComposerCli.php
+++ lib/core/Tiki/Package/ComposerCli.php
@@ -32,7 +32,7 @@ class ComposerCli
     ];
     public const PHP_MIN_VERSION = '7.4.0';
 
-    public const FALLBACK_COMPOSER_JSON = '{"minimum-stability": "stable","config": {"process-timeout": 5000,"bin-dir": "bin","component-dir": "vendor/components"}, "repositories": [{"type": "composer","url": "https://composer.tiki.org"}]}';
+    public const FALLBACK_COMPOSER_JSON = '{"minimum-stability": "stable","config": {"process-timeout": 5000,"bin-dir": "bin","component-dir": "vendor/components", "prepend-autoloader": false}, "repositories": [{"type": "composer","url": "https://composer.tiki.org"}]}';
 
     /**
      * @var string path to the base folder from tiki
