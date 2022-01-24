Olivier-Kango/tiki

commit b900524215eed78e60eb0b8aaa244902c8fc4a16

[ENH] Modules : Add new parameters to Latest Changes module (last_modif_pages)

modules/mod-func-last_modif_pages.php
@@ -33,6 +33,23 @@ function module_last_modif_pages_info()
                     'name' => tra('Show Namespace'),
                     'description' => tra('Show namespace prefix in page names.') . ' ( y / n )',    // Do not translate y/n
                     'default' => 'y'
+            ],
+            'date' => [
+                'name' => tra('Date'),
+                'description' => tra('If set to "y", show page edit dates.') . ' ( y / n )',
+            ],
+            'user' => [
+                'name' => tra('User'),
+                'description' => tra('If set to "y", show who edited the pages.') . ' ( y / n )',
+            ],
+            'comment' => [
+                'name' => tra('Comment'),
+                'description' => tra('If set to "y", show the descriptions of the change made on the pages.') . ' ( y / n )',
+            ],
+            'maxcomment' => [
+                'name' => tra('Maximum Length for comments'),
+                'description' => tra('Maximum number of characters in comments allowed before truncating.'),
+                'filter' => 'int'
             ]
         ],
         'common_params' => ['nonums', 'rows']
@@ -54,4 +71,14 @@ function module_last_modif_pages($mod_reference, $module_params)
     $smarty->assign('absurl', isset($module_params["absurl"]) ? $module_params["absurl"] : 'n');
     $smarty->assign('url', isset($module_params["url"]) ? $module_params["url"] : 'tiki-lastchanges.php');
     $smarty->assign('namespaceoption', isset($module_params['show_namespace']) ? $module_params['show_namespace'] : 'n');
+    $smarty->assign('date', isset($module_params["date"]) ? $module_params["date"] : 'n');
+    $smarty->assign('user', isset($module_params["user"]) ? $module_params["user"] : 'n');
+    $smarty->assign('comment', isset($module_params["comment"]) ? $module_params["comment"] : 'n');
+    $smarty->assign('maxcomment', isset($module_params["maxcomment"]) ? $module_params["maxcomment"] : 0);
+    // if one of the parameters exist and equal to "y"
+    if ((isset($module_params["date"]) && ($module_params["date"] == 'y')) || (isset($module_params["user"]) && ($module_params["user"] == 'y')) || (isset($module_params["comment"]) && ($module_params["comment"] == 'y'))) {
+        $smarty->assign('modLastModifTable', 'y');
+    } else {
+        $smarty->assign('modLastModifTable', 'n');
+    }
 }

templates/modules/mod-last_modif_pages.tpl
@@ -1,8 +1,88 @@
 {* $Id$ *}
 
 {tikimodule error=$module_params.error title=$tpl_module_title name="last_modif_pages" flip=$module_params.flip decorations=$module_params.decorations nobox=$module_params.nobox notitle=$module_params.notitle}
-    {modules_list list=$modLastModif nonums=$nonums}
-        {section name=ix loop=$modLastModif}
+    {if $modLastModifTable eq 'y'}
+        <div class="table-responsive">
+            <table class="table">
+                <tr>
+                    {if $date eq 'y'}<th>{tr}Date{/tr}</th>{/if}
+                    <th>{tr}Page{/tr}</th>
+                    {if $user eq 'y'}<th>{tr}User{/tr}</th>{/if}
+                    {if $comment eq 'y'}<th>{tr}Comment{/tr}</th>{/if}
+                </tr>
+
+                {section name=ix loop=$modLastModif}
+                    <tr>
+                        {if $date eq 'y'}
+                            <td>
+                                {if $prefs.jquery_timeago eq 'y'}{$sameday='n'}{else}{$sameday=$prefs.tiki_same_day_time_only}{/if}
+                                {$modLastModif[ix].lastModif|tiki_short_datetime:'':'n'}{if $prefs.wiki_authors_style ne 'lastmodif'}{/if}
+                            </td>
+                        {/if}
+                        <td>
+                            <a class="linkmodule"
+                                {if $absurl eq 'y'}
+                                    href="{$base_url}tiki-index.php?page={$modLastModif[ix].pageName|escape:"url"}"
+                                {else}
+                                    href="{$modLastModif[ix].pageName|sefurl}"
+                                {/if}
+                                {* jquery_timeago doesn't work in a title atribute *}
+                                {if $prefs.jquery_timeago eq 'y'}{$sameday='n'}{else}{$sameday=$prefs.tiki_same_day_time_only}{/if}
+                                title="{$modLastModif[ix].lastModif|tiki_short_datetime:'':'n'}{if $prefs.wiki_authors_style ne 'lastmodif'}, {tr}by{/tr} {$modLastModif[ix].user|username}{/if}{if (strlen($modLastModif[ix].pageName) > $maxlen) && ($maxlen > 0)}, {$modLastModif[ix].pageName|escape}{/if}"
+                            >
+                                {if $maxlen > 0}{* 0 is default value for maxlen eq to 'no truncate' *}
+                                    {if $namespaceoption eq 'n'}
+                                        {$data=$prefs.namespace_separator|explode:$modLastModif[ix].pageName}
+                                        {if empty($data['1'])}
+                                            {$pagename=$data['0']}
+                                        {else}
+                                            {$pagename=$data['1']}
+                                        {/if}
+                                        {$pagename|escape|truncate:$maxlen:"...":true}
+                                    {else}
+                                        {$data=$prefs.namespace_separator|explode:$modLastModif[ix].pageName}
+                                        {if sizeof($data) == 1}
+                                            {$pagename=$modLastModif[ix].pageName|escape}
+                                        {else}
+                                            {$pagename=$modLastModif[ix]|escape}
+                                        {/if}
+                                        {$pagename|truncate:$maxlen:"...":true}
+                                    {/if}
+                                {else}
+                                    {$data=$prefs.namespace_separator|explode:$modLastModif[ix].pageName}
+                                    {if $namespaceoption eq 'n'}
+                                        {if empty($data['1'])}
+                                            {$data['0']}
+                                        {else}
+                                            {$data['1']}
+                                        {/if}
+                                    {else}
+                                        {$modLastModif[ix].pageName|escape}
+                                    {/if}
+                                {/if}
+                            </a>
+                        </td>
+
+                        {if $user eq 'y'}
+                            <td>{$modLastModif[ix].user|username}</td>
+                        {/if}
+
+                        {if $comment eq 'y'}
+                            {if $maxcomment > 0}
+                                <td>{$modLastModif[ix].comment|truncate:$maxcomment:"...":true}</td>
+                            {else}
+                                <td>{$modLastModif[ix].comment}</td>
+                            {/if}
+                        {/if}
+                    </tr>
+                {/section}
+
+            </table>
+        </div>
+        <a class="linkmodule" style="margin-left: 20px" href="{$url}">...{tr}more{/tr}</a>
+    {else}
+        {modules_list list=$modLastModif nonums=$nonums}
+            {section name=ix loop=$modLastModif}
             <li>
                 <a class="linkmodule"
                     {if $absurl eq 'y'}
@@ -22,7 +102,7 @@
                                 {$pagename=$data['0']}
                             {else}
                                 {$pagename=$data['1']}
-                                {/if}
+                            {/if}
                             {$pagename|escape|truncate:$maxlen:"...":true}
                         {else}
                             {$data=$prefs.namespace_separator|explode:$modLastModif[ix].pageName}
@@ -48,7 +128,8 @@
 
                 </a>
             </li>
-        {/section}
-    {/modules_list}
-    <a class="linkmodule" style="margin-left: 20px" href="{$url}">...{tr}more{/tr}</a>
+            {/section}
+        {/modules_list}
+        <a class="linkmodule" style="margin-left: 20px" href="{$url}">...{tr}more{/tr}</a>
+    {/if}
 {/tikimodule}

commit 476ae980fee4ee0b81dcae8cf3be7c3304cb98ab

[FIX] modules: Sadly smarty variables default to being global so assigning 'n' to `{$user}` affects the `$user` global everywhere (regression from b9005242). So change it to `$modif_user` for this module. [should fix wish7959 thanks @Bsfez]

modules/mod-func-last_modif_pages.php
@@ -77,7 +77,7 @@ function module_last_modif_pages($mod_reference, $module_params)
     $smarty->assign('url', isset($module_params["url"]) ? $module_params["url"] : 'tiki-lastchanges.php');
     $smarty->assign('namespaceoption', isset($module_params['show_namespace']) ? $module_params['show_namespace'] : 'n');
     $smarty->assign('date', isset($module_params["date"]) ? $module_params["date"] : 'n');
-    $smarty->assign('user', isset($module_params["user"]) ? $module_params["user"] : 'n');
+    $smarty->assign('modif_user', isset($module_params["user"]) ? $module_params["user"] : 'n');
     $smarty->assign('action', isset($module_params["action"]) ? $module_params["action"] : 'n');
     $smarty->assign('comment', isset($module_params["comment"]) ? $module_params["comment"] : 'n');
     $smarty->assign('maxcomment', isset($module_params["maxcomment"]) ? $module_params["maxcomment"] : 0);

templates/modules/mod-last_modif_pages.tpl
@@ -68,7 +68,7 @@
                             <td>{$page.action}</td>
                         {/if}
 
-                        {if $user eq 'y'}
+                        {if $modif_user eq 'y'}
                             <td>{$page.user|username}</td>
                         {/if}
 