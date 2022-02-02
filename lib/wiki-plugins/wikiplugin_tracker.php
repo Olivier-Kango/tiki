Olivier-Kango/tiki

commit 537ebdc5b4649f33e61de8ff9d7fcb519116d33a

[ENH][UX] Adding an option in the trackers so the title of a text field is...

lib/wiki-plugins/wikiplugin_tracker.php
@@ -2183,43 +2183,58 @@ function wikiplugin_tracker($data, $params)
                         $back .= " width='" . $colwidth . "'";
                     }
 
-                    // If on a different row the label should use all available width
-                    $back .= '><label class="';
-                    if ($isTextOnSameRow) {
-                        $back .= $labelclass;
-                        if ($f['type'] == 'h') {
-                            $back .= ' h' . $f['options_map']['level'];
+                    $back .= '>';
+                    // From here start the content of a row per field returned
+                    // If placeholder is not enable we continue else we skip samerow as it is not needed
+                    if (empty($f['options_map']['labelasplaceholder'])) {
+                        // If on a different row the label should use all available width
+                        $back .= '<label class="';
+                        if ($isTextOnSameRow) {
+                            $back .= $labelclass;
+                            if ($f['type'] == 'h') {
+                                $back .= ' h' . $f['options_map']['level'];
+                            } else {
+                                $back .= ' col-form-label';
+                            }
                         } else {
-                            $back .= ' col-form-label';
+                            $back .= 'col-md-12';
                         }
-                    } else {
-                        $back .= 'col-md-12';
-                    }
-                    $back .= '" for="' . $f['ins_id'] . '">' . wikiplugin_tracker_name($f['fieldId'], tra($f['name']), $field_errors);
+                        $back .= '" for="' . $f['ins_id'] . '">' . wikiplugin_tracker_name($f['fieldId'], tra($f['name']), $field_errors);
 
-                    if ($showmandatory == 'y' and $f['isMandatory'] == 'y') {
-                        $back .= " <strong class='mandatory_star text-danger tips' title=':" . tra('This field is mandatory') . "' >*</strong> ";
-                    }
+                        if ($showmandatory == 'y' and $f['isMandatory'] == 'y') {
+                            $back .= " <strong class='mandatory_star text-danger tips' title=':" . tra('This field is mandatory') . "' >*</strong> ";
+                        }
                         $back .= '</label>';
                         // If use different lines, add a line break.
                         // Otherwise a new column
-                    if (! $isTextOnSameRow) {
-                        $back .= "<br/>";
+                        if (! $f['options_map']['labelasplaceholder']) {
+                            if (! $isTextOnSameRow) {
+                                $back .= "<br/>";
+                            } else {
+                                $back .= '<div class="' . $inputclass . ' tracker_input_value tracker_field' . $f['fieldId'] . '">';
+                            }
+                        }
+                        $back .= $renderedField;
                     } else {
-                        $back .= '<div class="' . $inputclass . ' tracker_input_value tracker_field' . $f['fieldId'] . '">';
+                        $back .= '<div class="col-form-label col-md-12' . ' tracker_input_value tracker_field' . $f['fieldId'];
+                        $back .= '">';
+                        $back .= $renderedField;
+                        // Not sure about the error line found at line 2177, should it be replaced somewhere to indicate the field name in case of error ?
+                        // .wikiplugin_tracker_name($f['fieldId'], tra($f['name']), $field_errors);
+                        $back .= '</div>'; // chibaguy added /divs
                     }
-
-                    $back .= $renderedField;
-                    $back .= '</div>'; // chibaguy added /divs
 
                     if ($f['type'] === 'j') {
                         $datepicker = true;
                     }
 
-                    if ($isTextOnSameRow) {
-                        $back .= '</div>';
+                    if (! $f['options_map']['labelasplaceholder']) {
+                        if ($isTextOnSameRow) {
+                            $back .= '</div>';
+                        }
                     }
                 }
+                $back .= '</div>'; // We close the form-group row
 
                 if ($f['type'] != 'S' && empty($tpl) && empty($wiki)) {
                     if ($showfieldsdesc == 'y' && $f['description']) {

lib/core/Tracker/Field/Text.php
@@ -82,6 +82,17 @@ class Tracker_Field_Text extends Tracker_Field_Abstract implements Tracker_Field
                         ],
                         'legacy_index' => 6,
                     ],
+                    'labelasplaceholder' => [
+                        'name' => tr('Use label as placeholder'),
+                        'description' => tr('Display the field name as a placeholder in the input field instead of separate label.'),
+                        'deprecated' => false,
+                        'filter' => 'int',
+                        'default' => 0,
+                        'options' => [
+                            0 => tr('No'),
+                            1 => tr('Yes'),
+                        ],
+                    ],
                 ],
             ],
         ];

templates/tiki-view_tracker.tpl
@@ -390,14 +390,16 @@
                                 !($field_value.type eq 's' and $field_value.name eq 'Rating')
                         }
                             <div class="form-group row">
-                                <label class="col-sm-3 {if $field_value.type eq 'h'}h{$field_value.options_map.level}{else}col-form-label{/if}">
-                                        {if $field_value.isMandatory eq 'y'}
-                                            {$field_value.name|tra} <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}">*</strong>
-                                        {else}
-                                            {$field_value.name|tra}
-                                        {/if}
-                                </label>
-                                <div class="col-sm-8">
+                                {if empty($field_value.options_map.labelasplaceholder)}
+                                    <label class="col-sm-3 {if $field_value.type eq 'h'}h{$field_value.options_map.level}{else}col-form-label{/if}">
+                                            {if $field_value.isMandatory eq 'y'}
+                                                {$field_value.name|tra} <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}">*</strong>
+                                            {else}
+                                                {$field_value.name|tra}
+                                            {/if}
+                                    </label>
+                                {/if}
+                                <div class="col-sm-{if empty($field_value.options_map.labelasplaceholder)}8{else}12{/if}">
                                     {trackerinput field=$field_value inTable=formcolor showDescription=y}
                                 </div>
                             </div>

templates/tiki-view_tracker_item.tpl
@@ -284,6 +284,7 @@
 
                             {foreach from=$ins_fields key=ix item=cur_field}
                                 <div class="form-group row">
+									{if ($cur_field.type eq 't' && $cur_field.options_map.labelasplaceholder == 0)}
                                     <label class="col-sm-3 {if $cur_field.type eq 'h'}h{$cur_field.options_map.level}{else}col-form-label{/if}">
                                         {$cur_field.name|tra|escape}
                                         {if $cur_field.isMandatory eq 'y'}
@@ -291,9 +292,13 @@
                                         {/if}
                                     </label>
                                     <div class="col-sm-9">
+										{else}
+										<div class="col-sm-12">
+											{/if}
                                         {trackerinput field=$cur_field item=$item_info inTable=formcolor showDescription=y}
+                                        </div>
                                     </div>
-                                </div>
+								</div>
                             {/foreach}
 
                         {else}

templates/trackerinput/layout_flat.tpl
@@ -9,12 +9,14 @@
 {$jscal = 0}
 {foreach from=$fields item=field}
     <div class="form-group">
-        <label for="trackerinput_{$field.fieldId|escape}" {if $field.type eq 'h'}class="h{$field.options_map.level}"{/if}>
-            {$field.name|tra|escape}
-            {if $field.isMandatory eq 'y'}
-                <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}">*</strong>
-            {/if}
-        </label>
+        {if $field.type eq 't' and empty($field.options_map.labelasplaceholder)}
+            <label for="trackerinput_{$field.fieldId|escape}" {if $field.type eq 'h'}class="h{$field.options_map.level}"{/if}>
+                {$field.name|tra|escape}
+                {if $field.isMandatory eq 'y'}
+                    <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}">*</strong>
+                {/if}
+            </label>
+        {/if}
         <div id="trackerinput_{$field.fieldId|escape}">
             {trackerinput field=$field item=$item}
             {if !empty($field.description) && $field.type ne 'S'}

templates/trackerinput/layout_tab.tpl
@@ -11,12 +11,14 @@
             {/if}
             {foreach from=$sect.fields item=field}
                 <div class="form-group">
-                    <label for="trackerinput_{$field.fieldId|escape}">
-                        {$field.name|tra|escape}
-                        {if $field.isMandatory eq 'y'}
-                            <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}">*</strong>
-                        {/if}
-                    </label>
+                    {if $field.type eq 't' and empty($field.options_map.labelasplaceholder)}
+                        <label for="trackerinput_{$field.fieldId|escape}">
+                            {$field.name|tra|escape}
+                            {if $field.isMandatory eq 'y'}
+                                <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}">*</strong>
+                            {/if}
+                        </label>
+                    {/if}
                     <div id="trackerinput_{$field.fieldId|escape}">
                         {trackerinput field=$field item=$item}
                         {if !empty($field.description) && $field.type ne 'S'}

templates/trackerinput/text.tpl
@@ -1,11 +1,24 @@
 {if $field.isMultilingual ne 'y'}
-    <div{if $field.options_map.prepend or $field.options_map.append} class="input-group"{/if}>
+    <div{if $field.options_map.prepend or $field.options_map.append or $field.options_map.labelasplaceholder} class="input-group"{/if}>
         {if $field.options_map.prepend}
             <span class="input-group-append">
                 <span class="input-group-text">{$field.options_map.prepend}</span>
             </span>
         {/if}
-        <input type="text" class="form-control" id="{$field.ins_id|replace:'[':'_'|replace:']':''}" name="{$field.ins_id}" {if $field.options_map.size}size="{$field.options_map.size}"{/if} {if $field.options_map.max}maxlength="{$field.options_map.max}"{/if} value="{$field.value|default:$field.defaultvalue|escape}">
+        <input type="text" class="form-control {if $field.options_map.labelasplaceholder}labelasplaceholder{/if}"
+                id="{$field.ins_id|replace:'[':'_'|replace:']':''}" name="{$field.ins_id}"
+                value="{$field.value|default:$field.defaultvalue|escape}"
+                {if $field.options_map.labelasplaceholder}placeholder="{$field.name}"{/if}
+                {if $field.options_map.size}size="{$field.options_map.size}"{/if}
+                {if $field.options_map.max}maxlength="{$field.options_map.max}"{/if}
+        >
+        {if $field.type eq 't' and $field.options_map.labelasplaceholder and $field.isMandatory eq 'y'}
+            <span class="input-group-append">
+                <span class="input-group-text">
+                    <strong class='mandatory_star text-danger tips' title=":{tr}This field is mandatory{/tr}" style="font-size: 100%">{icon name='asterisk'}</strong>
+                </span>
+            </span>
+        {/if}
         {if $field.options_map.append}
             <span class="input-group-append">
                 <span class="input-group-text">{$field.options_map.append}</span>
@@ -18,7 +31,7 @@
     </div>
 {else}
     {foreach from=$field.lingualvalue item=ling name=multi}
-        <label for="{$ling.id|escape}">{$ling.lang|langname}</label>
+        {if !$field.options_map.labelasplaceholder}<label for="{$ling.id|escape}">{$ling.lang|langname}</label>{/if}
         <div{if $field.options_map.prepend or $field.options_map.append} class="input-group"{/if}>
             {if !empty($field.options_map.prepend)}
                 <span class="input-group-append">
@@ -26,8 +39,12 @@
                 </span>
             {/if}
 
-            <input type="text" id="{$ling.id|escape}" name="{$field.ins_id}[{$ling.lang}]" value="{$ling.value|escape}" class="form-control"
-                {if $field.options_map.size}size="{$field.options_map.size}"{/if} {if $field.options_map.max}maxlength="{$field.options_map.max}"{/if}> {*@@ missing value*}
+            <input type="text" id="{$ling.id|escape}" name="{$field.ins_id}[{$ling.lang}]" value="{$ling.value|escape}"
+                class="form-control{if $field.options_map.labelasplaceholder} labelasplaceholder mb-2{/if}"
+                {if $field.options_map.labelasplaceholder} placeholder="{tr}{$field.name}{/tr} - {$ling.lang|langname}"{/if}
+                {if $field.options_map.size}size="{$field.options_map.size}"{/if}
+                {if $field.options_map.max}maxlength="{$field.options_map.max}"{/if}
+            >
 
             {if $field.options_map.append}
                 <span class="input-group-append">

commit 1e3a9c0e02a7f3c70a88df0ad2f44c7deb4d971e

[FIX] trackers: Remove extra close div tags breaking layout when using template files - regressions from 537ebdc5

lib/wiki-plugins/wikiplugin_tracker.php
@@ -2232,7 +2232,6 @@ function wikiplugin_tracker($data, $params)
                         }
                     }
                 }
-                $back .= '</div>'; // We close the form-group row
 
                 if ($f['type'] != 'S' && empty($tpl) && empty($wiki)) {
                     if ($showfieldsdesc == 'y' && $f['description']) {