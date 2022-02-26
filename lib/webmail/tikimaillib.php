Olivier-Kango/tiki

commit 7c157deecf5fc0df861a254457b01522256c9bc6

[FIX] email_footer preference not working on TikiTrackers.org 21.x and dev.tiki.org 23.x

lib/webmail/tikimaillib.php
@@ -292,7 +292,17 @@ class TikiMail
             $title = 'mail';
         } else {
             try {
+
+                $email_body = $this->mail->getBody();
+                if($prefs['email_footer'])
+                {
+                    $email_footer = "\r\n\n\n".$prefs['email_footer'];
+                    $new_body = $email_body.$email_footer;
+                    $this->mail->setBody($new_body);
+                }
+                
                 tiki_send_email($this->mail);
+
                 $title = 'mail';
                 $error = '';
             } catch (Laminas\Mail\Exception\ExceptionInterface $e) {

commit 54d0a9f6c06ce0907cea784796cf22bc176d7c06

[FIX] mail: Prevent `Object of class Laminas\Mime\Message could not be converted to string` fatal error when sending html format notification emails (e.g. from `{tracker ...  emailformat="html"}` - regression from 7c157dee (and backports?)

lib/webmail/tikimaillib.php
@@ -297,9 +297,21 @@ class TikiMail
             try {
                 $email_body = $this->mail->getBody();
                 if ($prefs['email_footer']) {
-                    $email_footer = "\r\n\n\n" . $prefs['email_footer'];
-                    $new_body = $email_body . $email_footer;
-                    $this->mail->setBody($new_body);
+                    if (is_string($email_body)) {
+                        $new_body = $email_body . PHP_EOL . PHP_EOL . $prefs['email_footer'];
+                        $this->mail->setBody($new_body);
+                    } else {
+                        foreach($email_body->getParts() as $part) {
+                            $content = $part->getContent();
+                            if ($part->getType() === 'text/html') {
+                                $content = str_replace('</body>', '<br><br>' . $prefs['email_footer'] . '</body>', $content);
+                            } else {    // hopefully plain text
+                                $content = $content . PHP_EOL . PHP_EOL . $prefs['email_footer'];
+                            }
+                            $part->setContent($content);
+                        }
+                        $this->mail->setBody($email_body);
+                    }
                 }
 
                 tiki_send_email($this->mail);