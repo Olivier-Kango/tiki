f485961eee2ae30b768c409fa65cbccfd4afe5d7
Jonny Bradley <jmail@nospaces.net>
2022-03-07 10:44:47 +0000

    [FIX] trackers: Fix odd display effect in Chrome etc when a filename on the left column wraps and parts of it spil into the right column. Weird bootstrap class conflict...

--- lib/core/Tracker/Field/Files.php
+++ lib/core/Tracker/Field/Files.php
@@ -486,7 +486,7 @@ class Tracker_Field_Files extends Tracker_Field_Abstract implements Tracker_Fiel
                 $ret = '<ol class="tracker-item-files">';
 
                 foreach ($this->getConfiguration('files') as $fileId => $file) {
-                    $ret .= '<li class="m-1">';
+                    $ret .= '<li>';
                     if ($prefs['vimeo_upload'] == 'y' && $this->getOption('displayMode') == 'vimeo') {
                         $ret .= smarty_function_icon(['name' => 'vimeo'], $smarty->getEmptyInternalTemplate());
                     } else {
