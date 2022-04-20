Olivier-Kango/tiki

commit 844c3b4eeeba338a1b37c81e831aaa1814972f10

[ENH][TRA] Cleanup of Translations Using the %0 Substitution in The Strings

templates/admin/include_security.tpl
@@ -12,7 +12,7 @@
 </div>
 
 {remarksbox type="tip" title="{tr}Tip{/tr}"}
-    {tr}Please see the <a class='alert-link' target='tikihelp' href='http://dev.tiki.org/Security'>Security page</a> on Tiki's developer site.{/tr}
+    {tr _0="<a class='alert-link' target='tikihelp' href='http://dev.tiki.org/Security'>" _1="</a>"}Please see the %0Security page%1 on Tiki's developer site.{/tr}
     {tr}See <a class="alert-link" href="tiki-admin_security.php" title="Security"><strong>Security Admin</strong></a> for additional security settings{/tr}.
 {/remarksbox}
 
@@ -95,8 +95,7 @@
                         {tr}Requires the OpenSSL PHP extension for encryption.{/tr} {tr}You have OpenSSL installed.{/tr}<br>
                     {else}
                         {remarksbox type="warning" title="{tr}Sodium is not loaded{/tr}"}
-                        {tr}User Encryption requires the PHP extension Sodium for encryption.
-                            You should activate Sodium before activating User Encryption{/tr}.
+                        {tr}User Encryption requires the PHP extension Sodium for encryption.{/tr}<br>{tr}You should activate Sodium before activating User Encryption{/tr}.
                         {/remarksbox}
                     {/if}
                     {tr}You may also want to add the Domain Password module somewhere.{/tr}<br>
@@ -164,7 +163,7 @@
 
         {tab name="{tr}Spam Protection{/tr}"}
             {remarksbox type="tip" title="{tr}Tip{/tr}"}
-                {tr}You can additionally protect from spam enabling the "<a href="http://doc.tiki.org/Forum+Admin#Forum_moderation" target="_blank" class="alert-link">moderation queue on forums</a>", or through <strong>banning</strong> multiple ip's from the "<a href="tiki-admin_actionlog.php" target="_blank" class="alert-link">Action log</a>", from "<a href="tiki-adminusers.php" target="_blank" class="alert-link">Users registration</a>", or from the "<a href="tiki-list_comments.php" target="_blank" class="alert-link">Comments moderation queue</a>" itself{/tr}.
+                {tr _0='<a href="http://doc.tiki.org/Forum+Admin#Forum_moderation" target="_blank" class="alert-link">' _1="</a>" _2="<strong>" _3="</strong>" _4='<a href="tiki-admin_actionlog.php" target="_blank" class="alert-link">' _5="</a>" _6='<a href="tiki-adminusers.php" target="_blank" class="alert-link">' _7="</a>" _8='<a href="tiki-list_comments.php" target="_blank" class="alert-link">' _9="</a>"}You can additionally protect from spam enabling the '%0moderation queue on forums%1', or through %2banning%3 multiple ip's from the '%4Action log%5', from '%6Users registration{/tr}%7', or from the '%8Comments moderation queue%9' itself{/tr}.
             {/remarksbox}
             <fieldset>
                 <legend>{tr}CAPTCHA{/tr}</legend>
@@ -249,7 +248,7 @@
 
         {tab name="{tr}Tokens{/tr}"}
             {remarksbox type="tip" title="{tr}Tip{/tr}"}
-                {tr}To manage tokens go to <a href="tiki-admin_tokens.php" class="alert-link">Admin Tokens</a> page. Tokens are also used for the Temporary Users feature (see <a href="tiki-adminusers.php" class="alert-link">Admin Users</a>).{/tr}
+                {tr _0='<a href="tiki-admin_tokens.php" class="alert-link">' _1="</a>" _2='<a href="tiki-adminusers.php" class="alert-link">' _3='</a>'}To manage tokens go to %0Admin Tokens%1 page. Tokens are also used for the Temporary Users feature (see %2Admin Users%3).{/tr}
             {/remarksbox}
             {preference name=auth_token_access}
             {preference name=auth_token_access_maxtimeout}
@@ -279,7 +278,7 @@
                         <br><em>{tr}If you use file for the signer passphrase, clear the preferences option just for security{/tr}</em>
                     </div>
                     {remarksbox type="tip" title="{tr}Note{/tr}"}
-                        {tr}The email of preference <a href="tiki-admin.php?page=general&alt=General" class="alert-link">'sender_email'</a> is used as signer key ID, and it must have both private and public key in the gnupg keyring.{/tr}
+                        {tr _0='<a href="tiki-admin.php?page=general&alt=General" class="alert-link">' _1="</a>"}The email of preference %0'sender_email'%1 is used as signer key ID, and it must have both private and public key in the gnupg keyring.{/tr}
                     {/remarksbox}
                 </div>
             </fieldset>

templates/admin/include_connect.tpl
@@ -59,11 +59,11 @@
                 {remarksbox type="info" title="{tr}New Feature{/tr}" icon="bricks"}
                     <p><em>{tr}Please note: Experimental - work in progress{/tr}</em></p>
                     <p>{tr}Tiki Connect is a way to let the Tiki project know how it is being used, and which parts people like or would like fixing (or explaining).{/tr}<br>
-                        {tr}Once enabled, when you click the '<strong>Send Info</strong>' button below you will be connected with <em>mother.tiki.org</em>, which is where the data will be collected.{/tr}
+                        {tr _0="<strong>" _1="</strong>" _2="<em>mother.tiki.org</em>"}Once enabled, when you click the '%0Send Info%1' button below you will be connected with %2, which is where the data will be collected.{/tr}
                     </p>
-                    <p>{tr}You can also send feedback about Tiki by checking the 'Feedback' checkbox(once Tiki Connect is enabled, next to the 'Preference Filters' bar above).{/tr}
-                        {tr}Icons will appear next to all the preferences where you can 'like', request a 'fix' or ask 'what is this for?'{/tr}<br>
-                        {tr}Your votes are sent when you connect with mother.tiki.org (currently only by clicking the '<strong>Send Info</strong>' button){/tr}
+                    <p>{tr}You can also send feedback about Tiki by checking the 'Provide Feedback' checkbox (once Tiki Connect is enabled), in the 'Preference Filters' dropdown above.{/tr}
+                        {tr}Icons will appear next to all the preferences where you can 'like', request a 'fix' or ask 'what is this for?' then.{/tr}<br>
+                        {tr _0="<strong>" _1="</strong>" _2="<em>mother.tiki.org</em>"}Your votes are sent when you connect with %2 (currently only by clicking the '%0Send Info%1' button).{/tr}
                 {/remarksbox}
                 {preference name="connect_feature"}
                 <div class="adminoptionboxchild" id="connect_feature_childcontainer">

templates/admin/include_fgal.tpl
@@ -176,7 +176,7 @@
             <br>
             <span class="form-text">{tr}Configuration for gallery listings{/tr}</span>
             {remarksbox title="Note"}
-                {tr}Changing these settings will <em>not</em> affect existing file galleries. These changes will apply <em>only</em> to new file galleries{/tr}.
+                {tr _0='<em>' _1='</em>'}Changing these settings %0will not affect%1 existing file galleries. These changes %0will apply only%1 to new file galleries.{/tr}
             {/remarksbox}
 
             {preference name=fgal_default_view}
@@ -273,7 +273,7 @@
                                     {tr}Use <strong>%1</strong> as the internal file name. For example, use <strong>strings %1</strong> to convert the document to text, using the Unix <strong>strings</strong> command{/tr}.
                                 </li>
                                 <li>
-                                    {tr}To delete a handler, leave the <strong>System Command</strong> field blank{/tr}.
+                                    {tr _0="<strong>" _1="</strong>"}To delete a handler, leave the %0System Command%1 field blank{/tr}.
                                 </li>
                             </ul>
                         </div>

templates/admin/include_general.tpl
@@ -24,7 +24,7 @@
                 {preference name=site_title_location}
                 {preference name=site_title_breadcrumb}
                 {remarksbox type="info" title="{tr}Themes{/tr}"}
-                    {tr}Go to the <a href="tiki-admin.php?page=look" class="alert-link">Look & Feel</a> section for additional site customization preferences{/tr}.
+                    {tr _0='<a href="tiki-admin.php?page=look" class="alert-link">' _1='</a>'}Go to the %0Look & Feel%1 section for additional site customization preferences{/tr}.
                 {/remarksbox}
             </fieldset>
             <fieldset>

templates/admin/include_list_sections.tpl
@@ -3,12 +3,12 @@
 {remarksbox type="tip" title="{tr}Tip{/tr}"}
     {tr}Enable/disable Tiki features in {/tr}<a class="alert-link" href="tiki-admin.php?page=features">{tr}Control Panels{/tr}&nbsp;{$prefs.site_crumb_seper}&nbsp;{tr}Features{/tr}</a>{tr}, but configure them elsewhere{/tr}.
     <br/>
-    {tr}See <strong>more options</strong> after you enable more <a class='alert-link' target='tikihelp' href='https://doc.tiki.org/Preference+Filters'>Preference Filters</a> above ({icon name="filter"}){/tr}.
+    {tr _0="<strong>" _1="</strong>" _2='<a class='alert-link' target='tikihelp' href='https://doc.tiki.org/Preference+Filters'>' _3='</a>' _4='{icon name="filter"}'}See %0more options%1 after you enable more %2Preference Filters%3 above (%4){/tr}.
 {/remarksbox}
 
 {if $show_system_configuration_warning}
     {remarksbox type="warning" title="{tr}Warning{/tr}"}
-    {tr}Tiki detected system configuration files with <strong>.ini</strong> extension, under the root folder of Tiki. It is recommended to change it to <strong>.ini.php</strong>.<br/>Check <strong><a href="https://doc.tiki.org/System-Configuration">https://doc.tiki.org/System-Configuration</a></strong> for examples.{/tr}
+    {tr _0="<strong>.ini</strong>" _1="<strong>.ini.php</strong>" _2='<strong><a href="https://doc.tiki.org/System-Configuration">https://doc.tiki.org/System-Configuration</a></strong>'}Tiki detected system configuration files with %0 extension, under the root folder of Tiki. It is recommended to change it to %1.{/tr}{tr}Check %2 for examples.{/tr}
     {/remarksbox}
 {/if}
 

templates/admin/include_login.tpl
@@ -503,11 +503,10 @@
                     <h3>{tr}Upload Word List for Processing{/tr}</h3>
                     <p>{tr}Words currently indexed{/tr}: {$num_indexed}</p>
 
-                    <p>{tr}You may create custom blacklists to better fit your needs. Start by uploading a word list.
-                        Then reduce that list to something that applies to your specific configuration and needs with the tools that appear below.{/tr}</p>
+                    <p>{tr}You may create custom blacklists to better fit your needs. Start by uploading a word list. Then reduce that list to something that applies to your specific configuration and needs with the tools that appear below.{/tr}</p>
 
-                    <p>{tr}Raw password files can be obtained from <a href="https://github.com/danielmiessler/SecLists/tree/master/Passwords" target="_blank">Daniel Miessler's Collection</a>.
-                        Tiki's defaut password blacklist files were generated from Missler's top 1 million password file.{/tr}</p>
+                    <p>
+                        {tr _0='<a href="https://github.com/danielmiessler/SecLists/tree/master/Passwords" target="_blank">' _1='</a>'}Raw password files can be obtained from %0Daniel Miessler's Collection%1.{/tr} {tr}Tiki's default password blacklist files were generated from Missler's top 1 million password file.{/tr}</p>
 
                         <input type="file" name="passwordlist" accept="text/plain" class="form-control mb-2" />
                         <div class="col-sm-4">
@@ -523,7 +522,7 @@
                                 class="btn btn-primary btn-sm"
                             >
                             {help desc="{tr}Text files with one word per line accepted.
-                            The word list will be converted to all lowe case. Duplicate entries will be removed.
+                            The word list will be converted to all lower case. Duplicate entries will be removed.
                             Typically passwords lists should be arranged with the most commonly used passwords first.{/tr}"}
                             <input
                                 type="submit"
@@ -532,7 +531,7 @@
                                 class="btn btn-danger btn-sm"
                                 onclick="confirmPopup('{tr}Delete temporary index?{/tr}')"
                             >
-                            {help desc="{tr}It is recommended that you delete indexed passwords from your database after your done generating your password lists.
+                            {help desc="{tr}It is recommended that you delete indexed passwords from your database after you're done generating your password lists.
                             They can take up quite a lot of space and serve no pourpose after processing is complete.{/tr}"}
                         </div>
 

templates/admin/include_packages.tpl
@@ -4,7 +4,7 @@
     {remarksbox type="warning" title="{tr}Composer not found{/tr}"}
     {tr}Composer could not be executed, so the automated check on the packages cannot be performed.{/tr}
     <br/>
-    {tr}In <a href="javascript:void(0)" class="install-composer alert-link">Diagnose</a> tab you can install composer.{/tr}
+    {tr _0='<a href="javascript:void(0)" class="install-composer alert-link">{tr}Diagnose{/tr}</a>' _1='</a>'}In %0Diagnose%1 tab you can install composer.{/tr}
     {/remarksbox}
 {/if}
 
@@ -190,13 +190,13 @@
                         {/if}
                             <h4>{tr}Install packages manually{/tr}</h4>
                         {if ! $composer_available}
-                            <p><strong>{tr}Make sure <code>composer</code> is installed.{/tr}</strong></p>
+                            <p><strong>{tr _0="<code>composer</code>"}Make sure %0 is installed.{/tr}</strong></p>
                             <p>
                                 {tr}Composer can be installed manually, in the host machine, by following the instructions from the{/tr}
                                 <a href="https://doc.tiki.org/Composer">Composer</a> {tr}website.{/tr}
                             </p>
                             <p>
-                                {tr}The script <code>setup.sh</code> that is included in the Tiki distribution can be run to make sure composer is installed and the file permissions are correctly applied. In this case, composer will be installed as <code>temp/composer.phar</code>.{/tr}
+                                {tr _0="<code>setup.sh</code>" _1="<code>temp/composer.phar</code>"}The script %0 that is included in the Tiki distribution can be run to make sure composer is installed and the file permissions are correctly applied. In this case, composer will be installed as %1.{/tr}
                                 {tr}Below is an example of how to do this in a Linux-like operating system:{/tr}<br>
                                 <code>bash ./setup.sh composer</code>
                             </p>
@@ -256,20 +256,20 @@
 
                         <h4>{tr}Install packages manually{/tr}</h4>
                         {if ! $composer_available}
-                            <p><strong>{tr}Make sure <code>composer</code> is installed.{/tr}</strong></p>
+                            <p><strong>{tr _0="<code>composer</code>"}Make sure %0 is installed.{/tr}</strong></p>
                             <p>
                                 {tr}Composer can be installed manually, in the host machine, by following the instructions from the{/tr}
                                 <a href="https://doc.tiki.org/Composer">Composer</a> {tr}website.{/tr}
                             </p>
                             <p>
-                                {tr}The script <code>setup.sh</code> that is included in the Tiki distribution can be run to make sure composer is installed and the file permissions are correctly applied. In this case, composer will be installed as <code>temp/composer.phar</code>.{/tr}
+                                {tr _0="<code>setup.sh</code>" _1="<code>temp/composer.phar</code>"}The script %0 that is included in the Tiki distribution can be run to make sure composer is installed and the file permissions are correctly applied. In this case, composer will be installed as %1.{/tr}
                                 {tr}Below is an example of how to do this in a Linux-like operating system:{/tr}<br>
                                 <code>bash ./setup.sh composer</code>
                             </p>
                         {/if}
-                        <p><strong>{tr}Make sure there is a <code>composer.json</code> file in the root of the website.{/tr}</strong></p>
+                        <p><strong>{tr _0="<code>composer.json</code>"}Make sure there is a %0 file in the root of the website.{/tr}</strong></p>
                         <p>
-                            {tr}If there is not already a <code>composer.json</code> file, then create one.{/tr}
+                            {tr _0="<code>composer.json</code>"}If there is not already a %0 file, then create one.{/tr}
                             {tr}The sample <code>composer.json.dist</code> that comes with Tiki can be used as a starting point.{/tr}
                             {tr}Below is an example of how to do this in a Linux-like operating system:{/tr} <br>
                             <code>cp composer.json.dist composer.json</code>

commit 88c1a5681da235fffa3bc5dd9db16c8041eee2c7

[FIX] translation: Repair fatally broken security control panel by removing stray {/tr} tag (blatant regression from 844c3b4ee - please test more!)

templates/admin/include_security.tpl
@@ -163,7 +163,7 @@
 
         {tab name="{tr}Spam Protection{/tr}"}
             {remarksbox type="tip" title="{tr}Tip{/tr}"}
-                {tr _0='<a href="http://doc.tiki.org/Forum+Admin#Forum_moderation" target="_blank" class="alert-link">' _1="</a>" _2="<strong>" _3="</strong>" _4='<a href="tiki-admin_actionlog.php" target="_blank" class="alert-link">' _5="</a>" _6='<a href="tiki-adminusers.php" target="_blank" class="alert-link">' _7="</a>" _8='<a href="tiki-list_comments.php" target="_blank" class="alert-link">' _9="</a>"}You can additionally protect from spam enabling the '%0moderation queue on forums%1', or through %2banning%3 multiple ip's from the '%4Action log%5', from '%6Users registration{/tr}%7', or from the '%8Comments moderation queue%9' itself{/tr}.
+                {tr _0='<a href="http://doc.tiki.org/Forum+Admin#Forum_moderation" target="_blank" class="alert-link">' _1="</a>" _2="<strong>" _3="</strong>" _4='<a href="tiki-admin_actionlog.php" target="_blank" class="alert-link">' _5="</a>" _6='<a href="tiki-adminusers.php" target="_blank" class="alert-link">' _7="</a>" _8='<a href="tiki-list_comments.php" target="_blank" class="alert-link">' _9="</a>"}You can additionally protect from spam enabling the '%0moderation queue on forums%1', or through %2banning%3 multiple ip's from the '%4Action log%5', from '%6Users registration%7', or from the '%8Comments moderation queue%9' itself{/tr}.
             {/remarksbox}
             <fieldset>
                 <legend>{tr}CAPTCHA{/tr}</legend>