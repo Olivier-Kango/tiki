Olivier-Kango/tiki

commit eba7723e0cff22e704c89068abe6bceaa0be3a18

[FIX] An error is displayed when adding a question to a FAQ

lib/core/TikiFilter.php
@@ -177,7 +177,7 @@ class TikiFilter
                 return new StripTags();
 
             /** HTML Permitted, String Return Type **/
-            case 'purifier':
+            case 'purifyer':
             case 'html':
                 // Test Return " :/g.,:|4hΔ δ_🍘コン onclick<b></b>"
                 // Strips non-valid HTML and potentially malicious HTML

commit 00d54238babf1adb037acc45cc83d92bb81fc5c3

[FIX] Revert eba7723e (from !1813) which broke unit test `TikiFilter_MapTest::testTikiFiltersExpectedOutput` (don't ever rename a filter because there's an incorrect use of it, fix the typo that's causing the warning please)

lib/core/TikiFilter.php
@@ -177,7 +177,7 @@ class TikiFilter
                 return new StripTags();
 
             /** HTML Permitted, String Return Type **/
-            case 'purifyer':
+            case 'purifier':
             case 'html':
                 // Test Return " :/g.,:|4hΔ δ_🍘コン onclick<b></b>"
                 // Strips non-valid HTML and potentially malicious HTML