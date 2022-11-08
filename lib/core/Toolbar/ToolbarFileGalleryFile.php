7f93f6e1840e624b61096c2c158278b348d68f1a
Jonny Bradley <jmail@nospaces.net>
2022-11-08 10:31:01 +0000

    [FIX] toolbars: Add missing constructor for ToolbarFileGalleryFile

--- lib/core/Toolbar/ToolbarFileGalleryFile.php
+++ lib/core/Toolbar/ToolbarFileGalleryFile.php
@@ -7,6 +7,20 @@ use TikiLib;
 class ToolbarFileGalleryFile extends ToolbarFileGallery
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->setLabel(tra('Choose or upload files'))
+            ->setIconName('upload')
+            ->setIcon(tra('img/icons/upload.png'))
+            ->setWysiwygToken('tikifile')
+            ->setMarkdownSyntax('tikifile')
+            ->setMarkdownWysiwyg('tikifile')
+            ->setType('FileGallery')
+            ->setClass('qt-filegal')
+            ->addRequiredPreference('feature_filegals_manager');
+    }
     public function getOnClick(): string
     {
         $smarty = TikiLib::lib('smarty');
