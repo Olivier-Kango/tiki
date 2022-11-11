06b08a766e7171997871ed19e4272ccbf232ff60
Gary Cunningham-Lee <gary_c@cunningham-lee.com>
2022-11-11 17:18:44 +0900

    [FIX][UI] bs5: Update menu module form labels to be more accurate and consistent.

--- modules/mod-func-menu.php
+++ modules/mod-func-menu.php
@@ -17,7 +17,7 @@ function module_menu_info()
         'params' => [
             'id' => [
                 'name' => tra('Menu'),
-                'description' => tra('Identifier of a menu (from tiki-admin_menus.php)'),
+                'description' => tra('Identifier of the menu (from tiki-admin_menus.php)'),
                 'filter' => 'int',
                 'profile_reference' => 'menu',
             ],
@@ -29,22 +29,22 @@ function module_menu_info()
             ],
             'type' => [
                 'name' => tra('Type'),
-                'description' => tra('Direction for menu: horiz or vert (default vert)'),
+                'description' => tra('Orientation of menu: horiz or vert (default vert)'),
                 'filter' => 'text',
             ],
             'bootstrap' => [
-                'name' => tra('Use Bootstrap menus'),
+                'name' => tra('Use Bootstrap menu'),
                 'description' => tra('') . ' ( y / n )',
                 'default' => 'y',
             ],
             'megamenu' => [
-                'name' => tra('Use Smartmenu Megamenu'),
+                'name' => tra('Use Smartmenu megamenu'),
                 'description' => tra('This is a Smartmenu that has a flattened structure of level 1 (Smartmenu preference must be turned on).') . ' ( y / n )',
                 'default' => 'n',
             ],
             'megamenu_static' => [
-                'name' => tra('Use Static Megamenu'),
-                'description' => tra('This is a full-width navbar-nav Megamenu') . ' ( y / n )',
+                'name' => tra('Align megamenu dropdown with the full width of the menu'),
+                'description' => tra('This is instead of the dropdown being in the standard position under its parent item in the menu') . ' ( y / n )',
                 'default' => 'y',
             ],
             // TODO - needs image url field
@@ -55,7 +55,7 @@ function module_menu_info()
             // ],
 
             'navbar_toggle' => [
-                'name' => tra('Show Navbar Toggle Button'),
+                'name' => tra('Show navbar toggle button'),
                 'description' => tra('Used in Bootstrap navbar menus when viewport is too narrow for menu items') . ' ( y / n )',
                 'default' => 'y',
             ],
@@ -71,7 +71,7 @@ function module_menu_info()
             ],
             'menu_id' => [
                 'name' => tra('DOM #id'),
-                'description' => tra('Id of the menu in the DOM'),
+                'description' => tra('HTML id of the menu in the DOM'),
             ],
             'menu_class' => [
                 'name' => tra('CSS class'),
@@ -79,37 +79,37 @@ function module_menu_info()
                 'filter' => 'text',
             ],
             'sectionLevel' => [
-                'name' => tra('Limit low visibles levels'),
-                'description' => tra('All the submenus beginning at this level will be displayed if the url matches one of the option of this level or above or below.'),
+                'name' => tra('Lower limiit on visible menu levels'),
+                'description' => tra('In a menu with many levels such as made with a wiki structure, menu levels lower than this won\'t be displayed.'),
                 'filter' => 'int',
             ],
             'toLevel' => [
-                'name' => tra('Limit top visible levels'),
-                'description' => tra('Do not display options higher than this level.'),
+                'name' => tra('Upper limit on visible menu levels'),
+                'description' => tra('Menu levels higher than this won\'t be displayed.'),
                 'filter' => 'int',
             ],
             'link_on_section' => [
-                'name' => tra('Link on Section'),
+                'name' => tra('Link on section'),
                 'description' => tra('Create links on menu sections') . ' ' . tra('(y/n default y)'),
                 'filter' => 'alpha',
             ],
             'translate' => [
                 'name' => tra('Translate'),
-                'description' => tra('Translate labels') . ' ' . tra('(y/n default y)'),
+                'description' => tra('Enable translation of menu text') . ' ' . tra('(y/n default y)'),
                 'filter' => 'alpha',
             ],
             'menu_cookie' => [
-                'name' => tra('Menu Cookie'),
+                'name' => tra('Menu cookie'),
                 'description' => tra('Open the menu to show current option if possible') . ' ' . tra('(y/n default y)'),
                 'filter' => 'alpha',
             ],
             'show_namespace' => [
-                'name' => tra('Show Namespace'),
+                'name' => tra('Show namespace'),
                 'description' => tra('Show namespace prefix in page names') . ' ( y / n )', // Do not translate y/n
                 'default' => 'y'
             ],
             'setSelected' => [
-                'name' => tra('Set Selected'),
+                'name' => tra('Set selected'),
                 'description' => tra('Process all menu items to show currently selected item and other dynamic states. Useful when disabled on very large menus where performance becomes an issue.') . ' ( y / n )',
                 'default' => 'y'
             ],
