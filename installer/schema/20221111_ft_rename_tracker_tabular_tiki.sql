df368ccf340f6195070cd0f87a6e5f3f3714d98f
alvinBM <alvinbauma10@gmail.com>
2022-11-28 14:09:16 +0000

    [ENH][UX][DB] Change name of Tracker Tabular to Tracker Import-Export part 2

---
+++ installer/schema/20221111_ft_rename_tracker_tabular_tiki.sql
@@ -0,0 +1,1 @@
+UPDATE `tiki_menu_options` SET `name` = 'Manage Import-Export Formats' WHERE `menuId` = 42 AND `name` = 'Manage Tabular Formats' AND `url` = 'tiki-tabular-manage' AND `position` = 810;
