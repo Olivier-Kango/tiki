08b1b5414a234d3a47cffdde066e31a053503977
Josaphat Imani <josaphat.imani97@gmail.com>
2023-03-26 06:30:27 +0200

    [FIX] Page selector field: Fixed redirecting when pasting an existing page

--- tiki-listpages.php
+++ tiki-listpages.php
@@ -331,7 +331,7 @@ if (! empty($multiprint_pages)) {
     // Exact match and single result, go to page directly
     if (count($listpages['data']) == 1 && ! $ts['ajax']) {
         $result = reset($listpages['data']);
-        if (TikiLib::strtolower($find) == TikiLib::strtolower($result['pageName'])) {
+        if (! $access->is_xml_http_request() && TikiLib::strtolower($find) == TikiLib::strtolower($result['pageName'])) {
             $wikilib = TikiLib::lib('wiki');
             header('Location: ' . $wikilib->sefurl($result['pageName'], '', $all_langs));
             exit;
