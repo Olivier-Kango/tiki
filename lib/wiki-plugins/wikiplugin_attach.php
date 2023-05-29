ecce44ba695399ce66e3c26fd7b31d6841d0fcca
Josaphat Imani <josaphat.imani97@gmail.com>
2023-05-29 17:06:48 +0000

    * Fixed strstr issue

--- lib/wiki-plugins/wikiplugin_attach.php
+++ lib/wiki-plugins/wikiplugin_attach.php
@@ -200,7 +200,7 @@ function wikiplugin_attach($data, $params)
         }
 
         // See if we're being called from a wiki page.
-        if (strstr($section_class, 'wiki_page')) {
+        if ($section_class && strstr($section_class, 'wiki_page')) {
             $atts_item_name = $_REQUEST["page"];
             $atts = $wikilib->list_wiki_attachments($atts_item_name, 0, -1, 'created_desc', '');
         }
