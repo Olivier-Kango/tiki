1df61b3408c8c39863f8bc32b78aaad9d724c56d
Victor Emanouilov <kroky6@gmail.com>
2023-10-16 12:44:13 +0300

    [FIX] dropping unique index before dropping columns from the table fixes Mariadb error

--- installer/schema/20230831_remove_calendarid_from_subscriptions_tiki.sql
+++ installer/schema/20230831_remove_calendarid_from_subscriptions_tiki.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `tiki_calendar_subscriptions` DROP `calendarId`;
 ALTER TABLE `tiki_calendar_subscriptions` DROP KEY `user`;
+ALTER TABLE `tiki_calendar_subscriptions` DROP COLUMN `calendarId`;
 ALTER TABLE `tiki_calendar_subscriptions` ADD `uri` VARCHAR(100) NOT NULL AFTER `subscriptionId`;
 ALTER TABLE `tiki_calendar_subscriptions` ADD KEY(`uri`);
