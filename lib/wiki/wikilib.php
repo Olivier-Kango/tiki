Olivier-Kango/tiki

commit f2425eb70acdb6f6db7cfdc8b7088759e7e063d9

[ENH] Page selector - tracker field type : do not create page automatically
Commits:
* [ENH] Page selector - tracker field type : do not create page automatically

* [ENH] Page selector - tracker field type : do not create page automatically

* [ENH] Page selector - tracker field type : do not create page automatically (fix unit test)

* [ENH] Page selector - tracker field type : do not create page automatically

* [ENH] Page selector - tracker field type : do not create page automatically

* [ENH] Page selector - tracker field type : do not create page automatically (fix unit test)

* [ENH] Page selector - tracker field type : do not create page automatically

See merge request tikiwiki/tiki!3968

lib/wiki/wikilib.php
@@ -1714,6 +1714,7 @@ class WikiLib extends TikiLib
         global $prefs, $info;
         $smarty = TikiLib::lib('smarty');
         $script_name = 'tiki-index.php';
+        $script_name2 = 'tiki-editpage.php';
 
         if ($prefs['feature_multilingual_one_page'] == 'y') {
             //  if ( basename($_SERVER['PHP_SELF']) == 'tiki-all_languages.php' ) {
@@ -1727,7 +1728,11 @@ class WikiLib extends TikiLib
 
         $pages = TikiDb::get()->table('tiki_pages');
         $page = $pages->fetchOne('pageSlug', ['pageName' => $page]) ?: $page;
-        $href = "$script_name?page=" . $page;
+        if (TikiLib::lib('tiki')->page_exists($page)) {
+            $href = "$script_name?page=" . $page;
+        } else {
+            $href = "$script_name2?page=" . $page;
+        }
 
         if (isset($prefs['feature_wiki_use_date_links']) && $prefs['feature_wiki_use_date_links'] == 'y') {
             if (isset($_REQUEST['date'])) {

lib/test/core/Search/FormatterTest.php
@@ -261,9 +261,21 @@ OUT;
             ]
         );
 
+        if (TikiLib::lib('tiki')->page_exists("HomePage")) {
+            $url1 = "tiki-index.php?page=HomePage";
+        } else {
+            $url1 = "tiki-editpage.php?page=HomePage";
+        }
+
+        if (TikiLib::lib('tiki')->page_exists("Some Page")) {
+            $url2 = "tiki-index.php?page=Some Page";
+        } else {
+            $url2 = "tiki-editpage.php?page=Some Page";
+        }
+
         $expect = <<<OUT
-* ~np~<a href="HomePage" class="" title="Home" data-type="wiki page" data-object="HomePage">Home</a>~/np~
-* ~np~<a href="Some Page" class="" title="Test" data-type="wiki page" data-object="Some Page">Test</a>~/np~
+* ~np~<a href="$url1" class="" title="Home" data-type="wiki page" data-object="HomePage">Home</a>~/np~
+* ~np~<a href="$url2" class="" title="Test" data-type="wiki page" data-object="Some Page">Test</a>~/np~
 
 OUT;
         $this->assertEquals($expect, $output);
@@ -288,11 +300,17 @@ OUT;
             ]
         );
 
+        if (TikiLib::lib('tiki')->page_exists("HomePage")) {
+            $url = "tiki-index.php?page=HomePage";
+        } else {
+            $url = "tiki-editpage.php?page=HomePage";
+        }
+
         $expect = <<<OUT
 <div>~np~<table>
     <caption>Count: 1</caption>
     <tr><th>Object</th><th>Type</th></tr>
-    <tr><td><a href="HomePage" class="" title="HomePage" data-type="wiki page" data-object="HomePage">HomePage</a></td><td>wiki page</td></tr>
+    <tr><td><a href="$url" class="" title="HomePage" data-type="wiki page" data-object="HomePage">HomePage</a></td><td>wiki page</td></tr>
 </table>
 ~/np~</div>
 OUT;

lib/trackers/trackerlib.php
@@ -6272,10 +6272,10 @@ class TrackerLib extends TikiLib
         foreach ($definition->getFields() as $field) {
             $fieldId = $field['fieldId'];
             $value = isset($args['values'][$fieldId]) ? $args['values'][$fieldId] : '';
-            if ($field['type'] == 'k' && $value != '' && ! empty($field['options'][2])) {
+            if ($field['type'] == 'k' && $value != '' && ! empty($field['options_array'][2])) {
                 if (! $this->page_exists($value)) {
                     $IP = $this->get_ip_address();
-                    $info = $this->get_page_info($field['options'][2]);
+                    $info = $this->get_page_info($field['options_array'][2]);
                     $tikilib->create_page($value, 0, $info['data'], $tikilib->now, '', $user, $IP, $info['description'], $info['lang'], $info['is_html'], [], $info['wysiwyg'], $info['wiki_authors_style']);
                 }
             }

commit 0c8035a5172be43db297158ac00624269b8714b2

[FIX] wiki: Don't look for the pageSlug in `page_exists` as it need the pageName
Commits:
* [FIX] wiki: Don't look for the pageSlug in `page_exists` as it need the pageName (regression from f2425eb7)

See merge request tikiwiki/tiki!4076

lib/wiki/wikilib.php
@@ -1727,8 +1727,8 @@ class WikiLib extends TikiLib
         }
 
         $pages = TikiDb::get()->table('tiki_pages');
-        $page = $pages->fetchOne('pageSlug', ['pageName' => $page]) ?: $page;
         if (TikiLib::lib('tiki')->page_exists($page)) {
+            $page = $pages->fetchOne('pageSlug', ['pageName' => $page]) ?: $page;
             $href = "$script_name?page=" . $page;
         } else {
             $href = "$script_name2?page=" . $page;