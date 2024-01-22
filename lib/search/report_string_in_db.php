54b6a1084c75f1860b7fe101a4d52d95dc8767dd
Merci Jacob <mercihabam@gmail.com>
2024-01-22 20:46:07 +0200

    [ENH] improve queries performance when searching for string occurences in the db

--- lib/search/report_string_in_db.php
+++ lib/search/report_string_in_db.php
@@ -12,7 +12,8 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 
 if (! empty($_POST['string_in_db_search'])) {
     $searchString = $_POST['string_in_db_search'];
-    $result = searchAllDB($searchString);
+    $searchTable = $_POST['string_in_db_search_table'];
+    $result = searchAllDB($searchString, $searchTable);
     $tableCount = tableCount($result);
     $smarty->assign('searchResult', $result);
     $smarty->assign('tableCount', $tableCount);
@@ -66,59 +67,74 @@ if (! empty($_POST['string_in_db_search'])) {
 /**
 *   return array (table, attribute, occurrence count)
 */
-function searchAllDB($search)
+function searchAllDB($search, $searchTable = null)
+{
+    global $tikilib;
+    $result = [];
+
+    if ($searchTable) {
+        $result = searchInTable($search, $searchTable);
+    } else {
+        $tablesSql = "show tables";
+        $tables = $tikilib->fetchAll($tablesSql);
+
+        foreach ($tables as $key => $val) {
+            $vals = array_values($val);
+            $table = $vals[0];
+            if (substr($table, 0, 6) == 'index_' && substr($table, 0, 10) !== 'index_pref') {
+                continue;
+            }
+            $result = array_merge($result, searchInTable($search, $table));
+        }
+    }
+    return $result;
+}
+
+function searchInTable($searchTerm, $table)
 {
     global $tikilib, $prefslib;
 
     $result = [];
-    $out = '';
 
-    $sql = "show tables";
-    $rs = $tikilib->fetchAll($sql);
     $preferenceTables = [
         'tiki_preferences'      => 'name',
         'tiki_user_preferences' => 'prefName',
     ];
 
-    foreach ($rs as $key => $val) {
-        $vals = array_values($val);
-        $table = $vals[0];
-        if (substr($table, 0, 6) == 'index_' && substr($table, 0, 10) !== 'index_pref') {
-            continue;
-        }
-        $sql2 = "SHOW COLUMNS FROM `$table`";
-        $rs2 = $tikilib->fetchAll($sql2);
-        foreach ($rs2 as $key2 => $val2) {
-            $toExclude = [];
-            $vals2 = array_values($val2);
-            $colum = $vals2[0];
-            $type = $vals2[1];
+    $sql2 = "SHOW COLUMNS FROM `$table`";
+    $rs2 = $tikilib->fetchAll($sql2);
+    foreach ($rs2 as $key2 => $val2) {
+        $toExclude = [];
+        $vals2 = array_values($val2);
+        $colum = $vals2[0];
+        $type = $vals2[1];
 
-            if (in_array($table, array_keys($preferenceTables)) && $colum == $preferenceTables[$table]) {
-                $preferences = TikiDb::get()->fetchAll("SELECT $preferenceTables[$table] FROM $table");
-                $toExclude = $prefslib->filterHiddenPreferences($preferences);
+        if (in_array($table, array_keys($preferenceTables)) && $colum == $preferenceTables[$table]) {
+            $preferences = TikiDb::get()->fetchAll("SELECT $preferenceTables[$table] FROM $table");
+            $toExclude = $prefslib->filterHiddenPreferences($preferences);
+        }
+
+        if (isTextType($type)) {
+            $sql_search_fields = [];
+            $qrySearch = '%' . $searchTerm . '%';
+            $args = [$qrySearch];
+            $sql_search_fields[] = "`" . $colum . "` like ?";
+            $sql_search = "select count(*) from `$table` where (";
+            $sql_search .= implode(" OR ", $sql_search_fields) . ')';
+
+            if (! empty($toExclude)) {
+                $sql_search .= ' AND `' . $colum . '` NOT IN (?)';
+                $args[] = implode(', ', $toExclude);
             }
 
-            if (isTextType($type)) {
-                $sql_search_fields = [];
-                $qrySearch = '%' . $search . '%';
-                $args = [$qrySearch];
-                $sql_search_fields[] = "`" . $colum . "` like ?"; // '%" . str_replace("'", "''", $search) . "%'";
-                $sql_search = "select * from `$table` where (";
-                $sql_search .= implode(" OR ", $sql_search_fields) . ')';
-
-                if (! empty($toExclude)) {
-                    $sql_search .= ' AND `' . $colum . '` NOT IN (?)';
-                    $args[] = implode(', ', $toExclude);
-                }
-
-                $rs3 = $tikilib->fetchAll($sql_search, $args);
-                if (! empty($rs3)) {
-                    $result[] = ['table' => $table, 'column' => $colum, 'occurrences' => count($rs3)];
-                }
+            $rs3 = $tikilib->fetchAll($sql_search, $args);
+            $count = array_values($rs3[0])[0];
+            if ($count > 0) {
+                $result[] = ['table' => $table, 'column' => $colum, 'occurrences' => $count];
             }
         }
     }
+
     return $result;
 }
 
