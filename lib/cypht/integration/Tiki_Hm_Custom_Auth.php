6c4ee08708ad43612a55c19c1341a2123745cdbe
Victor Emanouilov <kroky6@gmail.com>
2024-01-26 17:49:48 +0200

    [FIX] phpcs pipeline

--- lib/cypht/integration/Tiki_Hm_Custom_Auth.php
+++ lib/cypht/integration/Tiki_Hm_Custom_Auth.php
@@ -5,8 +5,10 @@
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-class Tiki_Hm_Custom_Auth extends Hm_Auth {
-    public function check_credentials($user, $pass) {
+class Tiki_Hm_Custom_Auth extends Hm_Auth
+{
+    public function check_credentials($user, $pass)
+    {
         $userlib = TikiLib::lib('user');
         list($isvalid, $user) = $userlib->validate_user($user, $pass);
         return $isvalid;
