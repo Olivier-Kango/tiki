0df56f3b0261eeab4ac320ca95c165e6cc4509d9
Benoit Grégoire <benoitg@coeus.ca>
2024-02-07 16:27:19 -0500

    [NEW] wikiplugin_semanticsearch proof of concept

---
+++ lib/wiki-plugins/wikiplugin_semanticsearch.php
@@ -0,0 +1,105 @@
+<?php
+
+// (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+function wikiplugin_semanticsearch_info()
+{
+    return [
+        'name' => tra('Semantic Search'),
+        'documentation' => 'PluginSemanticSearch',
+        'description' => tra('Create a custom search using the https://github.com/Textualization/php-semantic-search'),
+        'prefs' => ['wikiplugin_customsearch', 'feature_search'],
+        'format' => 'html',
+        'iconname' => 'search',
+        'introduced' => 27,
+        'tags' => ['advanced'],
+        'params' => [
+            'tpl' => [
+                'required' => false,
+                'name' => tra('Template file'),
+                'description' => tra('Smarty template (.tpl) file where search user interface template is found'),
+                'since' => '27',
+                'default' => 'wiki-plugins/wikiplugin_semanticsearch.tpl',
+                ],
+            'class' => [
+                'required' => false,
+                'name' => tra('Template file'),
+                'description' => tra('The textualization index class to instanciate.'),
+                'filter' => 'string',
+                'since' => '27',
+                'default' => '\\Textualization\\SemanticSearch\\VectorIndex',
+                ],
+            'dblocation' => [
+                'required' => true,
+                'name' => tra('Template file'),
+                'description' => tra('The filesystem location of the .db file to search into.'),
+                'since' => '27'
+                ],
+        ],
+    ];
+}
+
+function wikiplugin_semanticsearch($data, $params)
+{
+    global $prefs;
+    $defaults = [];
+    $plugininfo = wikiplugin_semanticsearch_info();
+    foreach ($plugininfo['params'] as $key => $param) {
+        $defaults["$key"] = $param['default'] ?? null;
+    }
+    $params = array_merge($defaults, $params);
+
+    $id = 'semanticsearch';
+    $html = '';
+    $location = $params['dblocation'];
+    if (! is_file($location)) {
+        throw new InvalidArgumentException("$location isn't a file");
+    }
+
+    $desc["location"] = $location;
+
+    $desc['class'] = $params['class'];
+
+    $desc["max_docs"] = 20;
+    $factory = "\Textualization\SemanticSearch\IndexFactory::make";
+    if (! is_callable($factory)) {
+        throw new Error("Unable to find $factory. You need to install the textualization/semantic-search composer package in your tiki for this plugin to work");
+    }
+    try {
+        $index = call_user_func($factory, $desc);
+    } catch (Exception $e) {
+        $msg = "Unable to initialise the semantic search index.  Note that the search index requires permission to the index files, and access to ffo in php.ini.  The actual error is: ";
+        return $msg . '<br/>' . $e;
+    }
+
+    $smarty = new Smarty_Tiki();
+    $smarty->assign('id', $id);
+    $query = $_REQUEST['query'];
+    $smarty->assign('query', $query);
+    $smarty->assign('facets', []);
+
+    $smartyResults = [];
+    if ($query) {
+        $results = $index->search($query);
+        foreach ($results as $result) {
+            //$html .= print_r($result, true);
+            $smartyResult = [];
+            //This is just so the template will parse
+            $smartyResult['object_type'] = 'wiki page' ;
+            //This is just so the template will parse
+            $smartyResult['object_id'] = 'dummyid';
+
+            $smartyResult['url'] = $result->url;
+            $smartyResult['title'] = $result->title;
+            $smartyResult['score'] = sprintf('%.2f', $result->score);
+            $chunkText = $index->fetch_document($result->url, $result->chunk_num)->text;
+            $smartyResult['highlight'] = substr($chunkText, 0, strrpos(substr($chunkText, 0, 300), ' '));
+            $smartyResults[] = $smartyResult;
+        }
+    }
+    $smarty->assign('results', $smartyResults);
+    $html .= $smarty->fetch($params['tpl']);
+    return $html;
+}
---
+++ templates/wiki-plugins/wikiplugin_semanticsearch.tpl
@@ -0,0 +1,48 @@
+{* This is meant to emulatethe wikiplugin_customsearch form  *}
+<div id="semanticsearch_{$id}_form">
+<form id="semanticsearch_{$id}" class="customsearch_form">
+    <div class="input-group mb-3">
+        <input id="q" name="query" type="text" class="form-control" placeholder="Any text..." value="{$query}">
+        <input type="Submit" value="Search" class="btn btn-outline-info">
+    </div>
+</form>
+</div>
+
+{* This is meant to emulate, and take the same parameters (when applicable) as searchresults-plain.tpl  *}
+<div>
+<ul class="searchresults">
+    {foreach item=result from=$results}
+    <li>
+        <strong>
+        {object_link type=$result.object_type id=$result.object_id title=$result.title url=$result.url}
+
+        {if $prefs.feature_search_show_object_type eq 'y'}
+            (<span class="objecttype">{tr}{$result.object_type|escape}{/tr}</span>)
+        {/if}
+
+        {if $prefs.feature_search_show_pertinence eq 'y' and !empty($result.score)}
+            <span class="itemrelevance">({tr}Relevance:{/tr} {$result.score|escape})</span>
+        {/if}
+
+        {if $prefs.feature_search_show_visit_count eq 'y' and $result.visits neq null}
+            <span class="itemhits">({tr}Visits:{/tr} {$result.visits|escape})</span>
+        {/if}
+
+        {if !empty($result.parent_object_id)} {tr}in{/tr} {object_link type=$result.parent_object_type id=$result.parent_object_id}{/if}
+
+        {if !empty($result._external)}<span class="label label-info">{tr}External{/tr}</span>{/if}
+        </strong>
+
+        <blockquote>
+            <p>{$result.highlight}</p>
+
+            {if $prefs.feature_search_show_last_modification eq 'y'}
+                <div class="searchdate small">{tr}Last modification:{/tr} {$result.modification_date|tiki_long_datetime}</div>
+            {/if}
+        </blockquote>
+    </li>
+    {foreachelse}
+            <li>{tr}No pages matched the search criteria test{/tr} </li>
+    {/foreach}
+</ul>
+</div>
