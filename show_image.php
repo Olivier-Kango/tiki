5afe67de0e32759ace98c8898b4723473678142f
Bienvenu Moringa <bienvenumoringa@gmail.com>
2024-03-05 16:42:41 +0000

    [FIX] show_image.php : fixing Undefined array key id --- * [FIX] show_image.php : fixing Undefined array key id
    See merge request tikiwiki/tiki!4531

--- show_image.php
+++ show_image.php
@@ -17,7 +17,12 @@ global $tiki_p_admin, $prefs, $tikilib, $tikipath;
 include_once("tiki-setup.php");
 
 if ($prefs['feature_file_galleries'] == 'y' && $prefs['file_galleries_redirect_from_image_gallery'] == 'y') {
+    if (! isset($_REQUEST["id"])) {
+        Feedback::error(tr('Request not processed, image identifier not provided'));
+        return;
+    }
     $fileInfo = $tikilib->table('tiki_object_attributes')->fetchRow([], ['value' => $_REQUEST["id"], 'attribute' => 'tiki.file.imageid']);
+
     if ($fileInfo) {
         include_once($tikipath . 'tiki-sefurl.php');
         $newUrl = filter_out_sefurl('tiki-download_file.php?fileId=' . $fileInfo['itemId'] . '&display');
