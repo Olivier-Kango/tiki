Olivier-Kango/tiki

commit 6cb235675d70b09652eb2e678d7b9d980e418611

[FIX] use modern jquery syntax for listening and triggering the submit event --- * [FIX] use modern jquery syntax for listening and triggering the submit event
See merge request tikiwiki/tiki!4574

lib/jquery_tiki/tiki-confirm.js
@@ -117,7 +117,7 @@ function confirmPopup(title, ticket) {
                 simpleConfirmForm(el, newForm, title, ticket).modal('show');
             //
             } else {
-                $(el.form).submit();
+                $(el.form).trigger("submit");
             }
         //if a link was clicked
         } else if (el.tagName === 'A') {
@@ -192,7 +192,7 @@ function simpleConfirmForm(clickedElement, newForm, title, ticket) {
         '<div class="modal-footer">' +
         '<button type="button" class="btn btn-primary btn-dismiss" data-bs-dismiss="modal">' + tr('Close') + '</button>' +
         '<input type="submit" class="btn btn-primary" value="' + tr('OK') +
-        '" onclick="$(\'#' + $(newForm).attr('id') + '\').submit(); return false;"> ' +
+        '" onclick="$(\'#' + $(newForm).attr('id') + '\').trigger("submit"); return false;"> ' +
         '</div>'
     );
     return target;

commit 5f21a8ccb6ea71d29fdfab1723f3e7dbf16337e3

Reduce the number of inputs on tiki-objectpermissions.php --- * [FIX] php8: Fatal array_diff error when no deleted perms
* [FIX] perms: This way seems to work, so only the changed checkbox data gets submitted so unlikley to be more than 1000 woo
(and `copy` and `paste` work server side so should be unaffected)

* [FIX] jq4: Inline function parameters need different quotes from the containing attribute (possible regression from 6cb23567)

* [FIX] perms: This doesn't work either, because the confirm popup ok button no longer functions once this has run, not sure why... take #3 coming next

* [FIX] perms: Permissions admin form has more inputs than the default number php allows, so remove ones that haven't been changed before subitting the form

See merge request tikiwiki/tiki!4549

lib/jquery_tiki/tiki-confirm.js
@@ -192,7 +192,7 @@ function simpleConfirmForm(clickedElement, newForm, title, ticket) {
         '<div class="modal-footer">' +
         '<button type="button" class="btn btn-primary btn-dismiss" data-bs-dismiss="modal">' + tr('Close') + '</button>' +
         '<input type="submit" class="btn btn-primary" value="' + tr('OK') +
-        '" onclick="$(\'#' + $(newForm).attr('id') + '\').trigger("submit"); return false;"> ' +
+        '" onclick="$(\'#' + $(newForm).attr('id') + '\').trigger(\'submit\'); return false;"> ' +
         '</div>'
     );
     return target;

tiki-objectpermissions.php
@@ -239,7 +239,7 @@ if (isset($_REQUEST['assign']) && ! isset($_REQUEST['quick_perms']) && $access->
         $isParentGroup = false;
         foreach ($groupInheritance as $index => $gi) {
             if (is_array($gi) && in_array($groupName, $gi)) {
-                $delPerms = $changed['deleted'][$groupList[$index]];
+                $delPerms = $changed['deleted'][$groupList[$index]] ?? [];
                 $changed['deleted'][$groupList[$index]] = array_diff($delPerms, $addPerms);
                 $isParentGroup = true;
             }
@@ -592,10 +592,14 @@ foreach ($groupNames as $groupName) {
 }   // end of for $groupNames loop
 
     // add cell colouring helpers
-    $js .= '
-$("table.objectperms input[type=checkbox]").on("change", function () {
-    var $this = $(this);
-    var $parent = $this.parent();
+    $js .= /** @lang JavaScript */
+        '
+const $objectPermTables = $("table.objectperms tbody");
+
+$("input[type=checkbox]", $objectPermTables).on("change", function () {
+    const $this = $(this);
+    const $parent = $this.parent();
+    $this.data("checked", $this.is(":checked"));
     if ($this.is(":checked")) {
         if ($parent.hasClass("removed")) {
             $parent.removeClass("removed");
@@ -610,6 +614,34 @@ $("table.objectperms input[type=checkbox]").on("change", function () {
         }
     }
 });
+
+/*** reduce the number of inputs by removing unchanged ones ***/
+
+const $objectPermsForm = $objectPermTables.parents("form");
+const $submitButtons = $("input[type=submit]", $objectPermsForm);
+
+const removeUnchangedCheckboxes = function () {
+    // this needs to remove the inputs from the confirm popup form as it runs before
+    $("input[name^=\'perm[\']", "form#confirm-popup").each(function() {
+        const $this = $(this),
+            $oldPermInput = $this.parent()
+                .find("input[name=\'old_" + $this.attr("name") + "\'][value=\'" + $this.val() + "\']");
+        
+        // old_perm[blah] is there when a perm is checked, so the back end can tell its been unchecked
+        // so if theyre both there then its still checked
+        if ($oldPermInput.length) {
+            $oldPermInput.remove();
+            $this.remove();
+        }
+    });
+
+};
+
+$submitButtons.on("click", function() {
+  removeUnchangedCheckboxes();
+  return true;
+});
+
 ';
 
 $headerlib->add_jq_onready($js);