d884ab0bf6a3ccb65bd18875ce801f71b01220e9
Steven ngesera <muhngesteven@gmail.com>
2024-04-10 22:36:23 +0000

    [ENH] DavX5 (DavDroid): Adding Tiki's CalDAV and CardDAV accounts should be doable in one operation (not two) --- See merge request tikiwiki/tiki!4816

---
+++ tiki-caldav-carddav.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package tikiwiki
+ */
+
+// (c) Copyright by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+use Tiki\SabreDav\BasicAuth;
+use Tiki\SabreDav\Utilities;
+
+define('TIKI_CALDAV', true);
+
+require_once 'tiki-setup.php';
+TikiLib::setExternalContext(true);
+$access->check_feature('feature_calendar');
+$authBackend = new BasicAuth();
+$server = Utilities::buildCaldavCardDavServer($authBackend);
+$server->start();
