1cc93dcb7fa863ed72cccff57fa337b7fa490ab7
Bienvenu Moringa <bienvenumoringa@gmail.com>
2024-10-20 15:15:03 +0000

    [FIX] tiki-admin_sync.php: fixing when clicking on show diff without... --- * [FIX] tiki-admin_sync.php: fixing when clicking on show diff without providingany Remote Server Address
    See merge request tikiwiki/tiki!6091

--- tiki-admin_sync.php
+++ tiki-admin_sync.php
@@ -22,11 +22,17 @@ $access->check_permission('tiki_p_admin');
 
 if (! empty($_REQUEST['submit'])) {
     try {
-        $client = new Services_ApiClient($_REQUEST['url']);
+        $url = $_REQUEST['url'];
+        if (empty($_REQUEST['url'])) {
+            Feedback::errorPage(['mes' => 'The Remote Server Address is missing. Please provide a valid address to continue.']);
+        }
+        if (! filter_var($url, FILTER_VALIDATE_URL)) {
+            Feedback::errorPage(['mes' => 'The provided Remote Server Address is not valid.']);
+        }
+
+        $client = new Services_ApiClient($url);
         $response = $client->get($client->route('export-sync'));
-
         $remote_content = $response['data'];
-
         $export_controller = new Services_Export_Controller();
         $local_content = $export_controller->dumpContent();
 
