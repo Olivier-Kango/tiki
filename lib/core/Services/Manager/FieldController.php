7858730f626eedd927aa2b497b60d2583b00d069
Boss Ibrahim Mussa Gregoire <ibmussafb@gmail.com>
2024-11-25 14:22:06 +0000

    [FIX] Tiki manager: used undefined itemId and fieldId variable to update the database prefix. --- * [FIX] Tiki manager: used undefined itemID and fieldID variable.
    See merge request tikiwiki/tiki!6423

--- lib/core/Services/Manager/FieldController.php
+++ lib/core/Services/Manager/FieldController.php
@@ -45,7 +45,7 @@ class Services_Manager_FieldController
         $db_prefix = $handler->getOption('newInstanceDbPrefix');
 
         if ($db_prefix) {
-            $db_prefix .= 'item' . $itemId . 'field' . $fieldId;
+            $db_prefix .= 'item' . $input->itemId->int() . 'field' . $input->fieldId->int();
         }
 
         // TODO: see if some other validation is needed (note that most of the valiation is in the actual create command and that error output is already passed to the user)
